3ad3df0a94c8b34fce1703d36ff247c0835bf936
Taras1326 <166243411+Taras1326@users.noreply.github.com>
2025-08-16 16:35:40 +0300

    dino-header.js-fruit

--- src/js/dino-modal.js
+++ src/js/dino-modal.js
@@ -17,35 +17,6 @@ document.addEventListener("DOMContentLoaded", () => {
     })();
   });
 
-  // (() => {
-  //   const refs = {
-  //     openModalBtn: document.querySelector("[data-modal-open]"),
-  //     closeModalBtn: document.querySelector("[data-modal-close]"),
-  //     modal: document.querySelector("[data-modal]"),
-  //   };
-  
-  //   if (!refs.openModalBtn || !refs.closeModalBtn || !refs.modal) {
-  //     console.error("Ошибка: один из элементов не найден!");
-  //     return;
-  //   }
-  
-  //   refs.openModalBtn.addEventListener("click", () => {
-  //     console.log("Открытие модалки");
-  //     refs.modal.classList.remove("is-hidden");
-  //   });
-  
-  //   refs.closeModalBtn.addEventListener("click", () => {
-  //     console.log("Закрытие модалки");
-  //     refs.modal.classList.add("is-hidden");
-  //   });
-  
-  //   refs.modal.addEventListener("click", (event) => {
-  //     if (event.target === refs.modal) {
-  //       console.log("Закрытие при клике вне окна");
-  //       refs.modal.classList.add("is-hidden");
-  //     }
-  //   });
-  // })();
   
 
   
